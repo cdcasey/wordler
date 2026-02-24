b25a4fcd15bfdc2607c0eb36b75a630e49ee7316
Chris Casey <cdcasey@users.noreply.github.com>
2026-02-23 20:07:41 -0600

    Separate out likely words and better handle greys

--- src/components/PossibleWords.tsx
+++ src/components/PossibleWords.tsx
@@ -1,45 +1,67 @@
 // import possibleWords from "@/lib/words.json";
-import possibleWords from "@/lib/nyt-words.json";
-// import possibleWords from "@/lib/possible-words.json";
+import allPossibleWords from "@/lib/nyt-words.json";
+import likelyPossibleWords from "@/lib/possible-words.json";
 import type { WordleState } from "@/lib/app-reducer.ts";
 
-export function PossibleWords({ wordleState }: { wordleState: WordleState }) {
-	const filteredPossibleWords = possibleWords.filter((possibleWord) => {
-		const word = possibleWord;
-		const { gray, yellow, green } = wordleState;
-		if (gray.some((letter) => word.includes(letter))) {
+function possibleWordFilter(word: string, state: WordleState) {
+	const { gray, yellow, green } = state;
+	const effectiveGray = gray.filter(
+		(letter) => !green.includes(letter) && !yellow.some((y) => y.letter === letter),
+	);
+	if (effectiveGray.some((letter) => word.includes(letter))) {
+		return false;
+	}
+
+	// 2) All green letters match position
+	for (let i = 0; i < green.length; i++) {
+		if (green[i] && word[i] !== green[i]) {
 			return false;
 		}
+	}
 
-		// 2) All green letters match position
-		for (let i = 0; i < green.length; i++) {
-			if (green[i] && word[i] !== green[i]) {
-				return false;
-			}
+	// 3) Yellow letters exist but not in guessed position
+	for (const y of yellow) {
+		if (!word.includes(y.letter) || word[y.position] === y.letter) {
+			return false;
 		}
+	}
 
-		// 3) Yellow letters exist but not in guessed position
-		for (const y of yellow) {
-			if (!word.includes(y.letter) || word[y.position] === y.letter) {
-				return false;
-			}
-		}
+	return true;
+}
 
-		return true;
+export function PossibleWords({ wordleState }: { wordleState: WordleState }) {
+	const filteredPossibleWords = allPossibleWords.filter((possibleWord) => {
+		return possibleWordFilter(possibleWord, wordleState);
+	});
+	const filteredLikelyWords = likelyPossibleWords.filter((likelyWord) => {
+		return possibleWordFilter(likelyWord, wordleState);
 	});
 
 	if (!filteredPossibleWords.length) return <p>No possible words left</p>;
 	if (filteredPossibleWords.length > 500) return <p>More than {filteredPossibleWords.length} words...</p>;
 
 	return (
-		<ul className="flex max-w-xl flex-wrap content-start gap-2">
-			{filteredPossibleWords.map((word) => {
-				return (
-					<li key={word} className="h-fit">
-						{word}
-					</li>
-				);
-			})}
-		</ul>
+		<div className="flex flex-col">
+			<h2 className="mb-2 font-bold">Likely</h2>
+			<ul className="flex max-w-xl flex-wrap content-start gap-2">
+				{filteredLikelyWords.map((word) => {
+					return (
+						<li key={word} className="h-fit">
+							{word}
+						</li>
+					);
+				})}
+			</ul>
+			<h2 className="mt-4 mb-2 font-bold">Possible</h2>
+			<ul className="flex max-w-xl flex-wrap content-start gap-2">
+				{filteredPossibleWords.map((word) => {
+					return (
+						<li key={word} className="h-fit">
+							{word}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
 	);
 }
